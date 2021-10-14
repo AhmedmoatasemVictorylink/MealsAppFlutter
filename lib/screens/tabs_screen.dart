21ef1d426e7fd40fc654c2eb6f3b7b92b43dcc1c
Ahmed Moatasem <amoatasem@victorylink.com>
2021-10-14 12:01:09 +0200

    Adding side menu or drawer to hoem screen

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,7 +1,9 @@
 
+
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/favorites_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabScreen extends StatefulWidget {
 
@@ -11,7 +13,10 @@ class TabScreen extends StatefulWidget {
 
 class _TabScreenState extends State<TabScreen> {
 
-final List<Widget> _pages = [CategoriesScreen(), FavoritesScreen()];
+final List<Map<String, dynamic>> _pages = [
+  {"page": CategoriesScreen(), "title": "Categories"},
+  {"page": FavoritesScreen(), "title": "Your Favorites"},
+  ];
 
 void _selectPage(int index) {
     setState(() {
@@ -24,19 +29,27 @@ int _selectedPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("meals"),
+      appBar: AppBar(title: Text(_pages[_selectedPageIndex]["title"]),
+      backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: _pages[_selectedPageIndex],
+      drawer: mainDrawer(),
+      body: _pages[_selectedPageIndex]["page"],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedPageIndex,
+        //type: BottomNavigationBarType.fixed,  default 
         items: [
           BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
             icon: Icon(Icons.category),
             label: "Categories",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.category),
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
             label: "Favorites",
           ),
         ],
